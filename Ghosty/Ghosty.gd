1fcbd06f2ae64caff6a3d40a34f4225b190a293e
Max Gallant <mcgallant72@gmail.com>
2018-12-09 21:08:44 -0700

    Player takes damage when ghosty hitsd

--- Ghosty/Ghosty.gd
+++ Ghosty/Ghosty.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var DIR_CHANGE_LIKELIHOOD = 0.9
 export var SPEED = 5
+export var GHOST_POWER = 1
 var dir =  Vector2(rand_range(-100,100), rand_range(-100,100))
 var moving = true
 var ray_hits = []
@@ -10,6 +11,8 @@ var ray_hits = []
 var lunge_direction = null
 var LUNGE_SPEED = 5
 var can_lunge = true
+
+var can_hit_player = true
 
 func _can_see_player():
 	for b in $SiteArea.get_overlapping_bodies():
@@ -36,7 +39,7 @@ func has_los_with_player_body(body):
 	var se_corner = body_pos + extents
 
 	for pos in [body_pos, nw_corner, sw_corner, ne_corner, se_corner]:
-		var ray_res = space_state.intersect_ray(global_position, pos, [self])
+		var ray_res = space_state.intersect_ray(global_position, pos, [self, $GhostyArea])
 		if ray_res and ray_res["collider"].get_name() == "Player":
 			ray_hits.append(ray_res.position)
 
@@ -50,6 +53,7 @@ func _lunge(dir):
 	$LungeTimer.start()
 	can_lunge = false
 	lunge_direction = dir
+
 
 func _process(delta):
 	
@@ -79,12 +83,20 @@ func _process(delta):
 	
 
 func _on_LungeTimer_timeout():
-	$LungeTimer.stop()
 	$LungeCooldown.start()
-	print("lunge over")
 	lunge_direction = null
 
 func _on_LungeCooldown_timeout():
-	$LungeCooldown.stop()
-	print("cooldowned")
 	can_lunge = true
+
+
+func _on_GhostyArea_body_entered(body):
+	if body.get_name() == "Player" and can_hit_player:
+		GAME_STATE.adjust_player_health(-GHOST_POWER)
+		$PlayerHitCooldown.start()
+		can_hit_player = false
+
+
+func _on_PlayerHitCooldown_timeout():
+	print("cooled down")
+	can_hit_player = true
