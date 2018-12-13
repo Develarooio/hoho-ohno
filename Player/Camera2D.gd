6979eff010270079f73807fd01fb6a59b68caa2d
Clayton Sweeten <cjsweeten101@gmail.com>
2018-12-13 12:43:45 -0700

    smooth camera a bit

--- Player/Camera2D.gd
+++ Player/Camera2D.gd
@@ -1,7 +1,15 @@
 extends Camera2D
 
+var smooth_zoom = 1
+var target_zoom = 1
+
+const ZOOM_SPEED = 10
+
 func _process(delta):
 	var insanity = (float(GAME_STATE.insanity)/100 + .6)
-	zoom.x = 1/insanity
-	zoom.y = 1/insanity
+	target_zoom = 1/insanity
+	
+	smooth_zoom = lerp(smooth_zoom, target_zoom, ZOOM_SPEED * delta)
+	if smooth_zoom != target_zoom:
+		set_zoom(Vector2(smooth_zoom, smooth_zoom))
 	
