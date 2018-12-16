c9ae8d1f8137601cdf756b7392c2d1b0ce67b83b
Clayton Sweeten <cjsweeten101@gmail.com>
2018-12-15 19:31:38 -0700

    add menu sprite

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,19 +1,8 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-
-func _on_Button_pressed():
-	get_tree().change_scene("res://Levels/MainLevel.tscn")
+func _process(delta):
+	if Input.is_action_pressed('ui_accept'):
+		get_tree().change_scene("res://Levels/MainLevel.tscn")
+	elif Input.is_action_pressed('shoot'):
+		get_tree().change_scene("res://Levels/MainLevel.tscn")
